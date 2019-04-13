e0b25fa68bf5bc6c819daa87ab944e9fc309f47d
Paul Knopf <pauldotknopf@gmail.com>
2019-04-13 14:47:39 -0400

    Updating to an alpha version that auto downloads a Qt runtime.

--- src/Features/Program.cs
+++ src/Features/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Qml.Net;
+using Qml.Net.Runtimes;
 
 namespace Features
 {
@@ -8,6 +9,8 @@ namespace Features
     {
         static int Main(string[] args)
         {
+            RuntimeManager.DiscoverOrDownloadSuitableQtRuntime();
+            
             QQuickStyle.SetStyle("Material");
 
             using (var application = new QGuiApplication(args))
